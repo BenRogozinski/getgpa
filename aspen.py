f92e262a95d1f9ca4601d57acc7894bbe880d64d
BenRogozinski <brogodev@gmail.com>
2025-06-04 20:21:44 -0500

    Yuhhhh

--- aspen.py
+++ aspen.py
@@ -1,95 +1,63 @@
 import requests
-from pwinput import pwinput
 from bs4 import BeautifulSoup
 
-GRADE_POINTS = {
-  "F": 0,
-  "D": 0.5,
-  "C": 1,
-  "B": 1.5,
-  "A": 2
-}
+GRADE_POINTS = { "F": 0, "D": 0.5, "C": 1, "B": 1.5, "A": 2 }
+TYPE_POINTS = { "R": 0, "H": 0.5, "A": 1 }
 
-TYPE_POINTS = {
-  "R": 0,
-  "H": 0.5,
-  "A": 1
-}
+class Aspen:
+  def __init__(self, username, password):
+    self.session = requests.Session()
+    self.session.hooks = {
+      "response": lambda r, *args, **kwargs: r.raise_for_status()
+    }
+    logon_resp = self.session.get("https://aspen.cps.edu/aspen/logon.do")
+    logon_soup = BeautifulSoup(logon_resp.text, features="html.parser")
+    struts_token = logon_soup.find(
+      "input",
+      {"name": "org.apache.struts.taglib.html.TOKEN"}
+    ).get("value")
 
-def main():
-  aspen_username = input("Aspen username: ")
-  aspen_password = pwinput("Aspen password: ")
-
-  print("Logging in to Aspen")
-
-  session = requests.Session()
+    auth_resp = self.session.post(
+      "https://aspen.cps.edu/aspen/logon.do",
+      data={
+        "org.apache.struts.taglib.html.TOKEN": struts_token,
+        "userEvent": "930",
+        "deploymentId": "aspen",
+        "username": username,
+        "password": password
+      },
+      allow_redirects=False
+    )
+    if auth_resp.status_code != 302:
+      raise ValueError("Authentication failed")
   
-  resp = session.get("https://aspen.cps.edu/aspen/logon.do")
+  def get_transcript_classes(self) -> dict[str, str]:
+    transcript_resp = self.session.get(
+      "https://aspen.cps.edu/aspen/transcriptList.do?navkey=myInfo.trn.list"
+    )
+    transcript_table = self._parse_datagrid(transcript_resp.text)
+    class_ids = {}
+    for row in transcript_table:
+      class_ids[row[4]] = row[2]
+    return class_ids
   
-  auth_resp = session.post(
-    "https://aspen.cps.edu/aspen/logon.do",
-    data={
-      "org.apache.struts.taglib.html.TOKEN": get_struts(resp.text),
-      "userEvent": "930",
-      "deploymentId": "aspen",
-      "username": aspen_username,
-      "password": aspen_password
-    },
-    allow_redirects=False
-  )
+  def get_current_grades(self) -> dict[str, str]:
+    classes_resp = self.session.get(
+      "https://aspen.cps.edu/aspen/portalClassList.do?navkey=academics.classes.list"
+    )
+    classes_table = self._parse_datagrid(classes_resp.text)
+    class_grades = {}
+    for row in classes_table:
+      if row[7]:
+        class_grades[row[1]] = row[7].split(" ")[-1]
+    return class_grades
   
-  if auth_resp.status_code == 302:
-    print("Getting transcript information")
-    transcript_resp = session.get("https://aspen.cps.edu/aspen/transcriptList.do?navkey=myInfo.trn.list")
-    transcript_soup = BeautifulSoup(transcript_resp.text, features="html.parser")
-    table = transcript_soup.find("div", id="dataGrid").find("table")
-    transcript_rows = table.find_all("tr", class_=["listCell", "listRowHeight"])
-    class_info = {}
-    for row in transcript_rows:
-      cells = row.find_all("td")
-      type = cells[2].text.strip()[-1]
-      description = cells[4].text.strip()
-      class_info[description] = type
-    
-    print("Getting current grades")
-    classes_resp = session.get("https://aspen.cps.edu/aspen/portalClassList.do?navkey=academics.classes.list")
-    classes_soup = BeautifulSoup(classes_resp.text, features="html.parser")
-    table = classes_soup.find("div", id="dataGrid").find("table")
-    class_rows = table.find_all("tr", class_=["listCell", "listRowHeight"])
-    grades = {}
-    for row in class_rows:
-      cells = row.find_all("td")
-      description = cells[1].text.strip()
-      grade_string = cells[7].text.strip()
-      if grade_string and description in class_info and class_info.get(description) != "N":
-        grades[description] = {
-          "grade": grade_string.split(" ")[1],
-          "type": class_info.get(description)
-        }
-    
-    print("Calculating GPA\n")
-    unweighted, weighted = get_gpa(grades)
-    print(f"Un-Weighted GPA: {unweighted:.3f}")
-    print(f"Weighted GPA: {weighted:.3f}")
-  else:
-    print("Authentication failed")
-    exit(1)
-
-def get_struts(document: str) -> str:
-  soup = BeautifulSoup(document, features="html.parser")
-  return soup.find("input", {"name": "org.apache.struts.taglib.html.TOKEN"}).get("value")
-
-def get_gpa(classes: dict[str, dict[str, str]]):
-  grade_points_unweighted = []
-  grade_points_weighted = []
-  for _class in classes:
-    unweighted_points = GRADE_POINTS.get(classes[_class]["grade"])
-    weighted_points = unweighted_points + TYPE_POINTS.get(classes[_class]["type"])
-    grade_points_unweighted.append(unweighted_points)
-    grade_points_weighted.append(weighted_points)
-  unweighted_gpa = sum(grade_points_unweighted) / len(grade_points_unweighted) * 2
-  weighted_gpa = sum(grade_points_weighted) / len(grade_points_weighted) * 2
-  return unweighted_gpa, weighted_gpa
-
-if __name__ == "__main__":
-  main()
+  @staticmethod
+  def _parse_datagrid(document: str) -> list[list[str]]:
+    soup = BeautifulSoup(document, features="html.parser")
+    table = soup.find("div", id="dataGrid").find("table")
+    rows = table.find_all("tr", class_=["listCell", "listRowHeight"])
+    row_data = []
+    for row in rows:
+      row_data.append([x.text.strip() for x in row.find_all("td")])
+    return row_data
